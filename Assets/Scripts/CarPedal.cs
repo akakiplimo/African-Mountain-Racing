2dcf7d0a171a8c01014ae2a91371b6800ae33323
A. Kiplimo <akakiplimo@gmail.com>
2022-11-29 08:54:08 +0300

    Car Pedals added and Sprite Images anchored

---
+++ Assets/Scripts/CarPedal.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CarPedal : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Sprite defaultGraphic;
+    public Sprite pressedGraphic;
+
+    public Image graphicComponent;
+
+    public UnityEvent onMouse;
+
+    bool isMouseDown;
+
+    void Start()
+    {
+        graphicComponent.sprite = defaultGraphic;
+    }
+
+    void Update()
+    {
+        if (isMouseDown)
+        {
+            onMouse.Invoke();
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isMouseDown = true;
+        graphicComponent.sprite = pressedGraphic;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isMouseDown = false;
+        graphicComponent.sprite = defaultGraphic;
+    }
+}
